a2faab42e54021614349a26e157a4cb4f367d1d3
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-05-10 09:02:39 +0900

    B=master
    [d]
    1. validation: length
    2. associations
    
    [n]
    1. Let's develop further

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,10 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
+  
+  # Validations
+  validates :content, :length => { :maximum => 140}
+  
+  # Associations
+  belongs_to :user
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+  
+  # Associations
+  has_many :microposts
+  
 end
